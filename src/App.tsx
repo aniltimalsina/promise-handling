ec5822c84d3a3b120b9ecbb6893958b49163358a
aniltimalsina <timalsinaanil96@gmail.com>
2023-09-30 15:47:13 -0400

    add asynchronous code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
 function App() {
-  return <div>App</div>;
+  const apiURL = "https://random.dog/woof.json";
+  const [dogImg, setDogImg] = useState([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      fetch(apiURL)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setDogImg(data);
+        })
+        .catch((err) => {
+          console.log("you got an error", err);
+        });
+    }, 5000);
+  }, []);
+
+  return (
+    <>
+      <div className={styles.title}>My Dog Collection</div>
+      <div className={styles.dogImage}>
+        <img src={dogImg.url} alt="Dog Image"></img>
+      </div>
+    </>
+  );
 }
 
 export default App;
